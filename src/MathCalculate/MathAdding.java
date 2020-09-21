7888c8e5caa8b8d3980ce6f70ff0e03ca5bc7740
github@cybertekschool.com <github@cybertekschool.com>
2020-09-21 11:18:02 -0500

    added done method

--- src/MathCalculate/MathAdding.java
+++ src/MathCalculate/MathAdding.java
@@ -24,6 +24,7 @@ public class MathAdding {
 				
 				int resultMult=helper.multiply();
 				System.out.println("the result of multipling is "+resultMult);
+				System.out.println("i am done github");
 			}
 	 }
 
